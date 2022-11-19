63c7c81e3ca78403bc5e7df69d6663938c8a37da
rafajr00 <101396541+rafajr00@users.noreply.github.com>
2022-11-18 22:33:37 -0300

    meu segundo commit

---
+++ tarefas.js
@@ -0,0 +1,73 @@
+let input = document.getElementById("input-principal")
+let button = document.getElementById("botao-adicionar")
+let tarefa = document.getElementById("nome-tarefa-id")
+let listaCompleta = document.getElementById("tarefas")
+
+let arrayDeTarefas = []
+recarregarTarefas()
+
+
+function mostrarTarefas() {
+    let novaLi = ""
+
+    arrayDeTarefas.forEach((tarefa, index) => {
+
+        novaLi = novaLi + `
+        <li class="item-tarefa ${tarefa.concluida == true ? "concluido" : ""}">
+<button class="botao-foguete" onclick="concluirTarefa(${index})">
+    <i class="fa-solid fa-rocket"></i>
+</button>
+    
+<p class="nome-tarefa ${tarefa.concluida == true ? "concluido" : ""}">${tarefa.tarefa}</p>
+
+<button class="botao-delete" onclick="deletarTarefa(${index})">
+    <i class="fas fa-trash"></i>
+</button>
+</li>`
+    })
+    listaCompleta.innerHTML = novaLi
+
+    localStorage.setItem("lista", JSON.stringify(arrayDeTarefas))
+
+
+}
+
+function deletarTarefa(index) {
+    arrayDeTarefas.splice(index, 1)
+
+    mostrarTarefas()
+
+}
+
+
+function adicionarTarefa() {
+    arrayDeTarefas.push({
+        tarefa: input.value,
+        concluida: false
+    })
+
+
+    mostrarTarefas()
+}
+
+function concluirTarefa(index) {
+    arrayDeTarefas[index].concluida = !arrayDeTarefas[index].concluida
+
+    mostrarTarefas()
+
+}
+function recarregarTarefas() {
+    let minhasTarefas = localStorage.getItem("lista")
+
+    arrayDeTarefas = JSON.parse(minhasTarefas)
+
+
+}
+
+
+
+button.addEventListener("click", adicionarTarefa)
+
+
+
+
